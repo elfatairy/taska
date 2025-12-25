982a2b930724ad61dc2a089937f124def1f24a3f
OmarHassan <omar13102005@gmail.com>
2025-12-25 09:45:44 +0200

    feat: implement RBAC for routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,56 +1,71 @@
-import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { cookies } from "next/headers"
-import { NextRequest, NextResponse } from "next/server"
-import { convexClient } from "@/lib/convex-client"
-import { api } from "@convex/_generated/api"
-import { ACCOUNT_COOKIE_NAME } from "@/lib/constants"
+import {
+  auth,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { convexClient } from "@/lib/convex-client";
+import { api } from "@convex/_generated/api";
+import { ACCOUNT_COOKIE_NAME } from "@/lib/constants";
 
-const isAuthenticatedRoutes = createRouteMatcher(['/dashboard(.*)'])
-const unauthenticatedRoutes = ['/login']
+const isAuthenticatedRoutes = createRouteMatcher(["/dashboard(.*)"]);
+const routesPermissions = {
+  "/dashboard/manage/users": ["CTO"],
+};
+const unauthenticatedRoutes = ["/login"];
 
-export default clerkMiddleware(async (_auth: typeof auth, request: NextRequest) => {
-  const { userId } = await _auth()
+export default clerkMiddleware(
+  async (_auth: typeof auth, request: NextRequest) => {
+    const { userId, sessionClaims } = await _auth();
+    
+    if (isAuthenticatedRoutes(request)) {
+      if (!userId) {
+        return NextResponse.redirect(new URL("/login", request.url));
+      }
+      const userRole = sessionClaims!.metadata.role!;
+      const routePermissions = routesPermissions[request.nextUrl.pathname as keyof typeof routesPermissions];
+      if (routePermissions && !routePermissions.includes(userRole)) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+      }
+    } else if (unauthenticatedRoutes.includes(request.nextUrl.pathname) && userId) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    
 
-  if (isAuthenticatedRoutes(request) && !userId) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    const response = NextResponse.next();
+
+    const cookieName = ACCOUNT_COOKIE_NAME;
+    const age = 60 * 60 * 24 * 30 * 3;
+
+    const existingCookie = (await cookies()).get(cookieName);
+    const valueToSet = existingCookie?.value || crypto.randomUUID();
+
+    response.cookies.set({
+      name: cookieName,
+      value: valueToSet,
+      path: "/",
+      maxAge: age,
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
+
+    if (!existingCookie) {
+      convexClient.action(api.account.initializeAccount, {
+        accountToken: valueToSet,
+      });
+    }
+
+    return response;
   }
-  
-  if (unauthenticatedRoutes.includes(request.nextUrl.pathname) && userId) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
-
-  const response = NextResponse.next()
-  
-  const cookieName = ACCOUNT_COOKIE_NAME
-  const age = 60 * 60 * 24 * 30 * 3
-  
-  const existingCookie = (await cookies()).get(cookieName)
-  const valueToSet = existingCookie?.value || crypto.randomUUID()
-
-  response.cookies.set({
-    name: cookieName,
-    value: valueToSet,
-    path: '/',
-    maxAge: age,
-    httpOnly: false,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-  })
-
-  if (!existingCookie) {
-    convexClient.action(api.account.initializeAccount, {
-      accountToken: valueToSet,
-    })
-  }
-
-  return response
-})
+);
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
